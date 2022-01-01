c1fc1a910f456eae92e352e770cc482d50b088c1
Mohamed Chiboub <Mohamed.chiboub@expensya.com>
2022-01-01 22:35:19 +0100

    updates

--- Front/src/app/pages/login/login.component.ts
+++ Front/src/app/pages/login/login.component.ts
@@ -29,9 +29,29 @@ export class LoginComponent implements OnInit {
   async submitLoginForm() {
     await this.loadingIndicator.present();
     this.authService.initAuthService();
-    this.authService.preSignIn().subscribe(data => {
-      console.log(data);
+    this.authService.preSignIn().subscribe(preSignInData => {
+      console.log(preSignInData);
+      const signInId = preSignInData.signInId;
+      const username = this.loginForm.value.username;
+      const password = this.loginForm.value.password;
+      this.authService.signIn(signInId, username, password).subscribe(signInData => {
+        console.log(signInData);
+        const authCode = signInData.authCode;
+        this.authService.postSignIn(authCode).subscribe(postSignInData => {
+          const accessToken = postSignInData.accessToken;
+          const refreshToken = postSignInData.refreshToken;
+          this.authService.setTokens(accessToken, refreshToken);
+          this.loadingIndicator.dismiss();
+        }, err => {
+          console.log('third step');
+          console.log(err);
+        });
+      }, err => {
+        console.log('second step');
+        console.log(err);
+      });
     }, err => {
+      console.log('first step');
       console.log(err);
     });
   }
--- Front/src/app/services/auth.service.ts
+++ Front/src/app/services/auth.service.ts
@@ -1,9 +1,9 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import hmacsha256 from 'crypto-js/hmac-sha256';
 import { v4 } from 'uuid';
-import * as jwt from 'jsonwebtoken';
-import Base64 from 'crypto-js/enc-base64';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import * as hmacsha256 from 'crypto-js/hmac-sha256';
+import * as Base64 from 'crypto-js/enc-base64';
 
 import { environment } from '../../environments/environment';
 
@@ -11,6 +11,8 @@ import { environment } from '../../environments/environment';
   providedIn: 'root'
 })
 export class AuthService {
+
+  jwtHelper: JwtHelperService;
 
   backendUrl: string = environment.backendUrl;
   sha256Secret: string = environment.sha265Secret;
@@ -22,15 +24,19 @@ export class AuthService {
 
   // signin
   preAuthorization: string;
+  signInId: string;
 
   // post-signin
+  authCode: string;
   postAuthorization: string;
   accessToken: string;
   refreshToken: string;
 
   connectedUserId: string;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this.jwtHelper = new JwtHelperService();
+  }
 
   initAuthService() {
     this.codeVerifier = v4();
@@ -41,14 +47,36 @@ export class AuthService {
   preSignIn() {
     this.preAuthorization = 'Bearer ' + btoa(this.clientId + ':' + this.codeChallenge);
 
-    return this.http.post<any>(`${this.backendUrl}/authorize`, null);
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    return this.http.post<any>(`${this.backendUrl}/authorize`, null, { headers: { 'Pre-Authorization': this.preAuthorization } });
   }
 
-  signIn() {
-
+  signIn(signInId, username, password) {
+    this.signInId = signInId;
+    return this.http.post<any>(`${this.backendUrl}/authenticate`, {
+      signInId,
+      username,
+      password
+    });
   }
 
-  postSignIn() {
+  postSignIn(authCode) {
+    this.authCode = authCode;
+    console.log(this.authCode, this.codeVerifier);
+    this.postAuthorization = 'Bearer ' + btoa(this.authCode + ':' + this.codeVerifier);
 
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    return this.http.post<any>(`${this.backendUrl}/oauth/token`, null, { headers: { 'Post-Authorization': this.postAuthorization } });
+  }
+
+  setTokens(accessToken, refreshToken) {
+    this.accessToken = accessToken;
+    this.refreshToken = refreshToken;
+    this.setUserId(this.accessToken);
+  }
+
+  setUserId(accessToken) {
+    const accessTokenPayload = this.jwtHelper.decodeToken(accessToken);
+    this.connectedUserId = accessTokenPayload.id;
   }
 }
