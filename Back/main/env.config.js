d5b75f5f93c3d200a83f7153abecb03012aa50a3
Mohamed Chiboub <Mohamed.chiboub@expensya.com>
2022-01-01 10:32:41 +0100

    updated cert config files

--- Back/main/env.config.js
+++ Back/main/env.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    'key-file': 'tls/example.lcom-key.pem',
-    'cert-file': 'tls/example.lcom.pem',
-    'dh-strongfile': 'tls/dhparam.pem',
+    'key-file': '/etc/letsencrypt/live/pmscot.me/privkey.pem',
+    'cert-file': '/etc/letsencrypt/live/pmscot.me/fullchain.pem',
+    'dh-strongfile': '/etc/letsencrypt/live/pmscot.me/dh-strong.pem',
     'main_db_url': 'mongodb+srv://chiboub:chiboub@db-cot.6pho3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
     'permissionLevels': ['Master', 'Member', 'Surfer']
 }
