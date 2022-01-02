350663d3db055dc3608ed08caf20b5945c063fb8
Mohamed Chiboub <Mohamed.chiboub@expensya.com>
2022-01-02 21:34:44 +0100

    fixes

--- Front/src/app/pages/stations/find-path/find-path.component.ts
+++ Front/src/app/pages/stations/find-path/find-path.component.ts
@@ -3,6 +3,7 @@ import { Geolocation } from '@capacitor/geolocation';
 import { StationsService } from 'src/app/services/stations.service';
 import * as Leaflet from 'leaflet';
 import 'leaflet-routing-machine';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-find-path',
@@ -12,6 +13,8 @@ import 'leaflet-routing-machine';
 export class FindPathComponent implements OnInit, OnDestroy {
   map: Leaflet.Map;
   stations: any[] = [];
+
+  existsStations = new BehaviorSubject(false);
 
   ionViewDidEnter() { this.leafletMap(); }
 
@@ -25,20 +28,26 @@ export class FindPathComponent implements OnInit, OnDestroy {
       this.stations = data;
     
       let waypoints = this.stations.filter(value => value.isEnabled).map(value => Leaflet.latLng(value.station_lat, value.station_long));
-      Geolocation.getCurrentPosition().then(userCoords => {
-        waypoints = [Leaflet.latLng(userCoords.coords.latitude, userCoords.coords.longitude), ...waypoints];
-        console.log(waypoints);
-        Leaflet.Routing.control({
-          waypoints: waypoints,
-        }).addTo(this.map);    
-      });
+      
+      if(waypoints.length) {
+        Geolocation.getCurrentPosition().then(userCoords => {
+          waypoints = [Leaflet.latLng(userCoords.coords.latitude, userCoords.coords.longitude), ...waypoints];
+          console.log(waypoints);
+          Leaflet.Routing.control({
+            waypoints: waypoints,
+          }).addTo(this.map);    
+        });
+        this.existsStations.next(true);
+      } else {
+        this.existsStations.next(false);
+      }
     });
 
   }
 
 
   constructor(private stationsService: StationsService) {
-    
+    this.existsStations.next(false);
   }
 
   ngOnInit() { }
