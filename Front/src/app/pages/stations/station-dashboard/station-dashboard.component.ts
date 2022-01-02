bf1eb4bc6bee935cd93d1fb27310dbb289733f19
Mohamed Chiboub <Mohamed.chiboub@expensya.com>
2022-01-02 17:30:49 +0100

    added map in station details

--- Front/src/app/pages/stations/station-dashboard/station-dashboard.component.ts
+++ Front/src/app/pages/stations/station-dashboard/station-dashboard.component.ts
@@ -1,28 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { StationsService } from 'src/app/services/stations.service';
+import * as Leaflet from 'leaflet';
+import { antPath } from 'leaflet-ant-path';
 
 @Component({
   selector: 'app-station-dashboard',
   templateUrl: './station-dashboard.component.html',
   styleUrls: ['./station-dashboard.component.scss'],
 })
-export class StationDashboardComponent implements OnInit {
-  station: {
-    station_name: 'test'
-  };
+export class StationDashboardComponent implements OnInit, OnDestroy {
+  station: any;
+  loadedData: boolean;
   stationId: any;
+  map: Leaflet.Map;
+
+  ionViewDidEnter() { this.leafletMap(); }
+
+  leafletMap() {
+    this.map = Leaflet.map('mapId').setView([28.644800, 77.216721], 5);
+    Leaflet.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
+      attribution: 'edupala.com',
+    }).addTo(this.map);
+
+    const markPoint = Leaflet.marker([28.644800, 77.594563], {
+      icon: Leaflet.icon({
+        iconUrl: 'assets/marker-icon.png',
+        shadowUrl: 'assets/marker-shadow.png',
+        iconSize: [38, 95], // size of the icon
+        shadowSize: [50, 64], // size of the shadow
+        iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+        shadowAnchor: [4, 62],  // the same for the shadow
+        popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+      })
+    });
+    markPoint.bindPopup('<p>Tashi Delek - Bangalore.</p>');
+    this.map.addLayer(markPoint);
+  }
 
   constructor(private route: ActivatedRoute, private stationsService: StationsService) {
+    this.loadedData = false;
     this.stationId = this.route.snapshot.params['stationId'];
-    console.log(this.stationId);
+  }
+
+  ngOnInit() {
     this.stationsService.getStationByStationId(this.stationId).subscribe(data => {
-      console.log(data);
-      this.station = data;
+      this.loadedData = true;
+      this.station = data[0];
+      console.log(this.station);
     });
   }
 
-  ngOnInit() { 
+  ngOnDestroy() {
+    this.map.remove();
   }
+
 
 }
