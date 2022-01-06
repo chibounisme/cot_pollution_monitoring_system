74da4969b12fa0aa1fdcae8f1695c4f9753177f8
Mohamed Chiboub <Mohamed.chiboub@expensya.com>
2022-01-06 16:18:10 +0100

    updates

--- Back/main/mqtt.client.js
+++ Back/main/mqtt.client.js
@@ -27,13 +27,14 @@ client.on('connect', async function () {
 })
 
 client.on('message', async (topic, message) => {
-    const data = JSON.parse(message);
-    const mqttData = await MQTTDataModel.saveMQTTData({ topic, payload: data.payload, sensorType: data.sensorType });
-    console.log('Received MQTT Data: ' + mqttData);
-
+    
     // update mqtt data 
     let station = await StationsModel.Station.findOne({ station_id: topic });
     if (station.isEnabled) {
+        const data = JSON.parse(message);
+        const mqttData = await MQTTDataModel.saveMQTTData({ topic, payload: data.payload, sensorType: data.sensorType });
+        console.log('Received MQTT Data: ' + mqttData);
+        
         if (station) {
             if(data.sensorType == 'microphone') {
                 station.avgMicrophonelevel = ((station.avgMicrophonelevel *  station.microphonePayloadCount) + data.payload) / (station.avgMicrophonelevel + 1);
